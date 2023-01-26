d28a72215517a942ed89aca48491dccf93a510ec
shareef <creazyhell@gmail.com>
2023-01-26 07:41:04 +0200

    added google verification

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
-
 </script>
 
 <template>
-  <main class="grid place-content-center text-4xl">
-    soon..
+  <main class="grid grid-cols-12 grid-rows-[max-content_1fr] place-content-center text-4xl">
+    <aside>
+    </aside>
   </main>
 </template>
